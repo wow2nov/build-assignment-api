b3ceaf925e85b97a4e4ae8a17adcd059c9bbc2c2
Supapond <supapondmaliwab@gmail.com>
2026-01-13 15:38:39 +0700

    created code assignment

--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://supawow:0211@localhost:5432/lms-assignment",
 });
 
 export default connectionPool;
